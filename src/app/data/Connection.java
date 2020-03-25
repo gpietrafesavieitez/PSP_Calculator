51b2a645fcc6652ad05b5f15a67b0b5de200b263
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-03-25 07:26:39 +0100

    fixed port number

--- src/app/data/Connection.java
+++ src/app/data/Connection.java
@@ -3,5 +3,5 @@ package app.data;
 public class Connection {
     public static final String SERVER_NAME = "CalcServer";
     public static final String SERVER_HOST = "localhost";
-    public static final short SERVER_PORT = 666;
+    public static final short SERVER_PORT = 7777;
 }
